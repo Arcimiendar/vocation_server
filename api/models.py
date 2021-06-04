7de074245816bf5cfbe16517a4e3f0d213c766b6
Artimendor <artimendor@yandex.ru>
2021-06-04 17:16:20 +0300

    add postgres and simple model SearchWord

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class SearchWord(models.Model):
+    word = models.CharField(max_length=512, primary_key=True)
+
--- vocation_server/settings.py
+++ vocation_server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'vocation_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vocation',
+        'USER': 'vocation',
+        'PASSWORD': 'vocation',
+        'HOST': '0.0.0.0',
+        'PORT': '5432'
     }
 }
 
